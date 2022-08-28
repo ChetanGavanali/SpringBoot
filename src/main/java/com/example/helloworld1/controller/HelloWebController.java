131632b1ccf47df174d0bade20bec846444e28d0
ChetanGavanali <chetangavanali@gmail.com>
2022-08-28 12:39:46 +0530

    Added: Get request with query param

--- src/main/java/com/example/helloworld1/controller/HelloWebController.java
+++ src/main/java/com/example/helloworld1/controller/HelloWebController.java
@@ -1,15 +1,20 @@
 package com.example.helloworld1.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.helloworld1.entity.User;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.web.bind.annotation.*;
+
+import javax.swing.text.html.parser.Entity;
 
 
 @RestController
-@RequestMapping("/hello")
 public class HelloWebController {
-
-    @RequestMapping(value = {"","/","/home"})
+    @RequestMapping(value= {"","/","/home"})
     public String sayHello() {
-        return "Hello From Bridgelabz";
+        return "Hello From Bridgelabz!!";
+    }
+    @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+    public String sayHello(@RequestParam(value = "name", defaultValue = "Chetan") String name) {
+        return "Hello" + name + "!";
     }
 }
