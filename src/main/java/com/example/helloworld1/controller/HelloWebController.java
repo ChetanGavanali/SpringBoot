fe1894117f94ddab0ee0cdf9c4623cd8deab9c37
ChetanGavanali <chetangavanali@gmail.com>
2022-08-28 12:44:48 +0530

    Added: Post requesting using request body

--- src/main/java/com/example/helloworld1/controller/HelloWebController.java
+++ src/main/java/com/example/helloworld1/controller/HelloWebController.java
@@ -21,4 +21,8 @@ public class HelloWebController {
     public String sayHelloParam(@PathVariable String name) {
         return "Hello" + name + "!";
     }
+    @PostMapping("/post")
+    public String sayHello(@RequestBody User user) {
+        return "Hello" + user.getFirstName() + " " + user.getLastName() + "!";
+    }
 }
